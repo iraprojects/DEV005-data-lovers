0f824ea7b1606c4a45a5ef94b29d157595b8cd4c
iraprojects <kristenirazu@gmail.com>
2023-03-03 12:38:18 -0600

    Aplicando estilos a la info de los personajes

--- src/main.js
+++ src/main.js
@@ -1,22 +1,32 @@
 //import { example } from './data.js';
-//import data from './data/harrypotter/harry.json' assert {type: 'json'}; 
+//import data from './data/harrypotter/harry.json' assert {type: 'json'};
 //console.log(example, data['characters']);
-import data from './data/harrypotter/harry.js';
-import listCH from './data/harrypotter/CharactersList.js';
+import data from "./data/harrypotter/harry.js";
+import listCH from "./data/harrypotter/CharactersList.js";
 
-const charactersName = document.querySelector('#characters-names');
-const characters = data.characters 
+const charactersName = document.querySelector("#p-characters");
+const characters = data.characters;
 
 characters.forEach((element, i) => {
-    if(listCH.charactersList.includes(element.name)){
-        charactersName.innerHTML += 
-            `<li id='ch${i}'>
-                Name: ${element.name}
-                Birth: ${element.birth}
-                House: ${element.house}
-            </li>`;
-
+    if (listCH.charactersList.includes(element.name)) {
+    /* charactersName.innerHTML += 
+        `<li id='ch${i}'>
+            Name: ${element.name}
+            Birth: ${element.birth}
+            House: ${element.house}
+        </li>`; */
+        charactersName.insertAdjacentHTML (
+            "afterend",
+            `<ul class='ul-ch' id='ch${i}'>
+                <li>Name: ${element.name}</li>
+                <li>Birth: ${element.birth}</li>
+                <li>House: ${element.house}</li>
+                <li>Species: ${element.species}</li>
+                <li>Gender: ${element.gender}</li>
+            </ul>`
+        );
     }
+
 });
 /* 
 + ' ' + element.birth*/
