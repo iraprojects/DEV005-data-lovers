42c1622d634e360a93c1320b70b92e138423a057
MayraGarciaM <stefagarcia.23@hotmail.com>
2023-03-07 11:31:07 -0500

    Insertar img de los personajes

--- src/main.js
+++ src/main.js
@@ -5,26 +5,26 @@ const listElement = document.getElementById("list");
 listElement.style.display = "none";
 const loadingDiv = document.getElementById("loading")
 
-const charactersName = document.querySelector("#p-characters");
+const charactersName = document.getElementById("characters-names");
 var data;
 apiHarry().then((listHarry) => {
     loadingDiv.style.display = "none";
     listElement.style.display= "block";
-   
+   console.log(listHarry)
     listHarry.characters.map((item, index) => {
+        console.log(item.name)
         const valitate = listCH.charactersList.find((itemCH) => itemCH.name == item.name)
+        console.log(valitate)
         if(valitate){
-            charactersName.insertAdjacentHTML (
-                "afterend",
-                `<ul class='ul-ch' id='ch${i}'>
+            charactersName.innerHTML+=
+                `<ul class='ul-ch' id='ch${index}'>
                     <li>Name: ${item.name}</li>
                     <li>Birth: ${item.birth}</li>
                     <li>House: ${item.house}</li>
                     <li>Species: ${item.species}</li>
                     <li>Gender: ${item.gender}</li>
-                    <li> <img src="${valitate.link}">: </li>
+                    <li> <img src="${valitate.link}"> </li>
                 </ul>`
-            )
         }
     })
     /* 
