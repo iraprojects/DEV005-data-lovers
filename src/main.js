c1da5e09adacf87e621cdd81038139fcc83d4802
MayraGarciaM <stefagarcia.23@hotmail.com>
2023-03-06 19:42:37 -0500

    agregar img al listado

--- src/main.js
+++ src/main.js
@@ -1,24 +1,32 @@
-//import { example } from './data.js';
-//import data from './data/harrypotter/harry.json' assert {type: 'json'}; 
-//console.log(example, data['characters']);
-import data from './data/harrypotter/harry.js';
+import { apiHarry } from './api/apiHarry.js';
 import listCH from './data/harrypotter/CharactersList.js';
 
+const listElement = document.getElementById("list");
+listElement.style.display = "none";
+const loadingDiv = document.getElementById("loading")
+
 const charactersName = document.querySelector('#characters-names');
-const characters = data.characters 
-
-characters.forEach((element, i) => {
-    if(listCH.charactersList.includes(element.name)){
-        charactersName.innerHTML += 
-            `<li id='ch${i}'>
-                Name: ${element.name}
-                Birth: ${element.birth}
-                House: ${element.house}
-                Species: ${element.species}
-                Gender: ${element.gender}
+var data;
+apiHarry().then((listHarry) => {
+    loadingDiv.style.display = "none";
+    listElement.style.display= "block";
+   
+    listHarry.characters.map((item, index) => {
+        const valitate = listCH.charactersList.find((itemCH) => itemCH.name == item.name)
+        if(valitate){
+            charactersName.innerHTML += 
+            `<li id='ch${index}'>
+                Name: ${item.name}
+                Birth: ${item.birth}
+                House: ${item.house}
+                Species: ${item.species}
+                Gender: ${item.gender}
+                <img src="${valitate.link}" />
             </li>`;
+        }
+    })
+    /* 
+    + ' ' + element.birth*/
+    data = listHarry
+})
 
-    }
-});
-/* 
-+ ' ' + element.birth*/
